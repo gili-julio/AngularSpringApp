6976b6f127d76924844a6fd2599d1c13fd19e7f6
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-11-10 19:18:10 -0300

    feat: creat dashboard component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: AppComponent},
     {path: 'login', component: LoginComponent, pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent, pathMatch: 'full'},
 ];
