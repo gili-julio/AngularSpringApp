3c05c8b834e6d245e0525fe303a5361373f5b43b
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-11-11 10:20:13 -0300

    feat: integrate websocket service

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])) // Register functional interceptor
+    provideHttpClient(withInterceptors([authInterceptor])), provideAnimationsAsync() // Register functional interceptor
   ],
 });
