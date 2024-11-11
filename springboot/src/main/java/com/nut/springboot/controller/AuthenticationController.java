c89882b8208da8adef3c38ff04be95d540238803
gili-julio <giliardoplay@gmail.com>
2024-11-10 22:41:56 -0300

    feat: create DTO AuthResponse

--- springboot/src/main/java/com/nut/springboot/controller/AuthenticationController.java
+++ springboot/src/main/java/com/nut/springboot/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.nut.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nut.springboot.dto.AuthResponse;
 import com.nut.springboot.security.JwtUtil;
 
 import lombok.Getter;
@@ -25,11 +27,12 @@ public class AuthenticationController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/auth")
-    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+    public ResponseEntity<AuthResponse> generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
-             authenticationManager.authenticate(
+            authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-            return jwtUtil.generateToken(authRequest.getUsername());
+            String token = jwtUtil.generateToken(authRequest.getUsername());
+            return ResponseEntity.ok(new AuthResponse(token)); // Retorna o token encapsulado em AuthResponse
         } catch (AuthenticationException e) {
             throw new Exception("Credenciais inválidas", e);
         }
